57d10081debd3bd8aee1b670954c014e8f680429
Rafa Gómez <rgomezcasas@gmail.com>
2024-05-29 18:45:08 +0200

    feat(3-2): add base

---
+++ 03-reactive/2-domain_events/databases/shop.sql
@@ -0,0 +1,81 @@
+CREATE SCHEMA shop;
+
+CREATE TABLE shop.users (
+	id UUID PRIMARY KEY NOT NULL,
+	name VARCHAR(255) NOT NULL,
+	email VARCHAR(255) NOT NULL,
+	profile_picture VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE shop.products (
+	id UUID PRIMARY KEY,
+	name VARCHAR NOT NULL,
+	price_amount NUMERIC NOT NULL,
+	price_currency VARCHAR NOT NULL,
+	image_urls TEXT[] NOT NULL,
+	latest_top_reviews JSONB
+);
+
+CREATE TABLE shop.product_reviews (
+	id UUID PRIMARY KEY,
+	user_id UUID REFERENCES shop.users(id) NOT NULL,
+	product_id UUID REFERENCES shop.products(id) NOT NULL,
+	rating FLOAT NOT NULL,
+	comment VARCHAR(500)
+);
+
+
+-----------------------------------------
+-- VIEWS
+-----------------------------------------
+CREATE VIEW shop.product_with_reviews AS
+SELECT
+	p.id,
+	p.name,
+	p.price_amount AS amount,
+	p.price_currency AS currency,
+	p.image_urls,
+	COALESCE(
+		json_agg(
+			json_build_object(
+					'userName', u.name,
+					'userProfilePictureUrl', u.profile_picture,
+					'reviewRating', r.rating,
+					'reviewComment', r.comment
+			) ORDER BY r.rating DESC
+		) FILTER (WHERE r.id IS NOT NULL AND r.rating >= 4), '[]'
+	) AS latest_top_reviews
+FROM shop.products p
+	LEFT JOIN shop.product_reviews r ON p.id = r.product_id
+	LEFT JOIN shop.users u ON r.user_id = u.id
+GROUP BY p.id;
+
+
+-----------------------------------------
+-- TRIGGERS
+-----------------------------------------
+CREATE OR REPLACE FUNCTION update_latest_top_reviews_on_review_inserted() RETURNS TRIGGER AS $$
+BEGIN
+	UPDATE shop.products p
+	SET latest_top_reviews = (
+		SELECT json_agg(
+		   json_build_object(
+				'userName', u.name,
+				'userProfilePictureUrl', u.profile_picture,
+				'reviewRating', r.rating,
+				'reviewComment', r.comment
+		   ) ORDER BY r.rating DESC
+		)
+		FROM shop.product_reviews r JOIN shop.users u ON r.user_id = u.id
+		WHERE r.product_id = p.id AND r.rating >= 4
+	)
+	WHERE p.id = NEW.product_id;
+
+	RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER set_latest_top_reviews_TRIGGER
+	AFTER INSERT OR UPDATE ON shop.product_reviews
+	FOR EACH ROW
+EXECUTE FUNCTION update_latest_top_reviews_on_review_inserted();
---
+++ 03-reactive/2-domain_events/src/contexts/retention/user/infrastructure/PostgresRetentionUserRepository.ts
@@ -0,0 +1,46 @@
+import { Service } from "diod";
+
+import { PostgresConnection } from "../../../shared/infrastructure/PostgresConnection";
+import { UserId } from "../../../shop/users/domain/UserId";
+import { RetentionUser } from "../domain/RetentionUser";
+import { RetentionUserRepository } from "../domain/RetentionUserRepository";
+
+type DatabaseUser = {
+	id: string;
+	last_activity_date: Date;
+};
+
+@Service()
+export class PostgresRetentionUserRepository extends RetentionUserRepository {
+	constructor(private readonly connection: PostgresConnection) {
+		super();
+	}
+
+	async save(user: RetentionUser): Promise<void> {
+		const userPrimitives = user.toPrimitives();
+		const date = `${userPrimitives.lastActivityDate.toISOString().split("T")[0]} ${
+			userPrimitives.lastActivityDate.toISOString().split("T")[1].split(".")[0]
+		}`;
+
+		const query = `
+			INSERT INTO retention.users (id, last_activity_date)
+			VALUES ('${userPrimitives.id}', '${date}');`;
+
+		await this.connection.execute(query);
+	}
+
+	async search(id: UserId): Promise<RetentionUser | null> {
+		const query = `SELECT id, last_activity_date FROM retention.users WHERE id = '${id.value}';`;
+
+		const result = await this.connection.searchOne<DatabaseUser>(query);
+
+		if (!result) {
+			return null;
+		}
+
+		return RetentionUser.fromPrimitives({
+			id: result.id,
+			lastActivityDate: result.last_activity_date,
+		});
+	}
+}
