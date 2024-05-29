008a1da8b645a00e8177ee02b0c95fc01ba4937b
Rafa Gómez <rgomezcasas@gmail.com>
2024-05-29 16:19:12 +0200

    feat(1-1): validate review rating

--- 01-what_is_happening/1-from_use_case/1-context/src/contexts/shop/product_reviews/domain/ProductReviewRating.ts
+++ 01-what_is_happening/1-from_use_case/1-context/src/contexts/shop/product_reviews/domain/ProductReviewRating.ts
@@ -1,3 +1,9 @@
+import { ProductReviewRatingInvalidError } from "./ProductReviewRatingInvalidError";
+
 export class ProductReviewRating {
-	constructor(public readonly value: number) {}
+	constructor(public readonly value: number) {
+		if (value < 0 || value > 5) {
+			throw new ProductReviewRatingInvalidError(value);
+		}
+	}
 }
