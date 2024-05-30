969b839273403e7b6fab89a10a8f1d212585f43f
Rafa Gómez <rgomezcasas@gmail.com>
2024-05-30 10:38:59 +0200

    feat(1-1): clean lesson

--- 01-what_is_happening/1-context/src/contexts/retention/email/domain/EmailSender.ts
+++
@@ -1,5 +0,0 @@
-import { Email } from "./Email";
-
-export abstract class EmailSender {
-	abstract send<T extends Email>(email: T): Promise<void>;
-}
--- 01-what_is_happening/1-context/src/contexts/shared/infrastructure/dependency_injection/diod.config.ts
+++ 01-what_is_happening/1-context/src/contexts/shared/infrastructure/dependency_injection/diod.config.ts
@@ -1,13 +1,5 @@
 import { ContainerBuilder } from "diod";
 
-import { SendWelcomeEmailOnUserRegistered } from "../../../retention/email/application/send_welcome_email/SendWelcomeEmailOnUserRegistered";
-import { WelcomeEmailSender } from "../../../retention/email/application/send_welcome_email/WelcomeEmailSender";
-import { EmailSender } from "../../../retention/email/domain/EmailSender";
-import { FakeEmailSender } from "../../../retention/email/infrastructure/FakeEmailSender";
-import { UpdateLastActivityDateOnUserUpdated } from "../../../retention/user/application/update_last_activity_date/UpdateLastActivityDateOnUserUpdated";
-import { UserLastActivityUpdater } from "../../../retention/user/application/update_last_activity_date/UserLastActivityUpdater";
-import { RetentionUserRepository } from "../../../retention/user/domain/RetentionUserRepository";
-import { PostgresRetentionUserRepository } from "../../../retention/user/infrastructure/PostgresRetentionUserRepository";
 import { EventBus } from "../../domain/event/EventBus";
 import { UuidGenerator } from "../../domain/UuidGenerator";
 import { DomainEventFailover } from "../event_bus/failover/DomainEventFailover";
@@ -25,13 +17,5 @@ builder.registerAndUse(PostgresConnection);
 builder.registerAndUse(RabbitMqConnection);
 builder.registerAndUse(DomainEventFailover);
 builder.register(EventBus).use(RabbitMqEventBus);
-
-builder.registerAndUse(SendWelcomeEmailOnUserRegistered).addTag("subscriber");
-builder.registerAndUse(WelcomeEmailSender);
-builder.register(EmailSender).use(FakeEmailSender);
-
-builder.registerAndUse(UpdateLastActivityDateOnUserUpdated).addTag("subscriber");
-builder.registerAndUse(UserLastActivityUpdater);
-builder.register(RetentionUserRepository).use(PostgresRetentionUserRepository);
 
 export const container = builder.build();
